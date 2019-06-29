ba1dbc29e5456c0329ad9ca91e4de77b2b9029da
Egor Zaplatin <lawliet-94@mail.ru>
2019-06-29 20:50:02 +0300

    add webpack

--- src/routes/achievements/index.svelte
+++ src/routes/achievements/index.svelte
@@ -1,21 +1,87 @@
-<script context="module">
-    export function preload({ params, query }) {
-        return this.fetch(`achievements.json`).then(r => r.json()).then(list => {
-            return { list };
-        });
-    }
-</script>
-
 <script>
     import AchievementsList from '../../components/AchievementsList.svelte';
 
-    export let list;
+    // temp
+    export let list = [
+        {
+            "id": "mmb",
+            "title": "Московский марш бросок (ММБ)",
+            "decription": "",
+            "logo": "imgs/logo-placeholder.png",
+            "photos": [
+                "imgs/mmb/1.jpg"
+            ],
+            "date": "Wed Jun 19 2019 18:43:00 GMT+0300 (Москва, стандартное время)",
+            "unlocked": true,
+            "type": 2
+        },
+        {
+            "id": "Гонка героев",
+            "title": "Гонка героев",
+            "decription": "",
+            "logo": "imgs/logo-placeholder.png",
+            "photos": [
+                "photo-placeholder.jpg"
+            ],
+            "date": "Wed Jun 19 2019 18:43:00 GMT+0300 (Москва, стандартное время)",
+            "unlocked": true,
+            "type": 0
+        },
+        {
+            "id": "Диплом бакалавриата",
+            "title": "Диплом бакалавриата",
+            "decription": "",
+            "logo": "imgs/logo-placeholder.png",
+            "photos": [
+                "photo-placeholder.jpg"
+            ],
+            "date": "Wed Jun 19 2019 18:43:00 GMT+0300 (Москва, стандартное время)",
+            "unlocked": true,
+            "type": 0
+        },
+        {
+            "id": "",
+            "title": "Диплом магистратуры",
+            "decription": "",
+            "logo": "imgs/logo-placeholder.png",
+            "photos": [
+                "photo-placeholder.jpg"
+            ],
+            "date": "Wed Jun 19 2019 18:43:00 GMT+0300 (Москва, стандартное время)",
+            "unlocked": true,
+            "type": 0
+        },
+        {
+            "id": "",
+            "title": "Водительские права",
+            "decription": "",
+            "logo": "imgs/logo-placeholder.png",
+            "photos": [
+                "photo-placeholder.jpg"
+            ],
+            "date": "Wed Jun 19 2019 18:43:00 GMT+0300 (Москва, стандартное время)",
+            "unlocked": true,
+            "type": 0
+        },
+        {
+            "id": "",
+            "title": "Первая работа front-end разработчиком",
+            "decription": "",
+            "logo": "imgs/logo-placeholder.png",
+            "photos": [
+                "photo-placeholder.jpg"
+            ],
+            "date": "Wed Jun 19 2019 18:43:00 GMT+0300 (Москва, стандартное время)",
+            "unlocked": true,
+            "type": 0
+        }
+    ];
 </script>
 
 <svelte:head>
     <title>Achievements list</title>
 </svelte:head>
 
-<a href="add">Добавить достижение</a>
+<!--<a href="add">Добавить достижение</a>-->
 <AchievementsList list={list} />
 
