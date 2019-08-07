350f234850c566c7aeced3179e78b72fa1895270
Egor Zaplatin <lawliet-94@mail.ru>
2019-08-07 20:40:44 +0300

    list draft styling

--- src/client/components/AchievementsList.svelte
+++ src/client/components/AchievementsList.svelte
@@ -16,6 +16,18 @@
             day: 'numeric',
         })
     }
+
+    function getItemClasses(achievement) {
+        const mainClass = "achievement-list__item";
+        const classesMap = {
+            0: "achievement-list__item--bronze",
+            1: "achievement-list__item--silver",
+            2: "achievement-list__item--gold",
+            3: "achievement-list__item--brilliant"
+        };
+
+        return [mainClass, classesMap[achievement.type], ...!achievement.unlocked ? ["achievement-list__item--locked"] : []].join(" ")
+    }
 </script>
 
 <style>
@@ -23,9 +35,37 @@
         list-style: none;
     }
 
-    .achievement {
+    .achievement-list__item {
         display: flex;
         align-items: center;
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        margin-top: 20px;
+        max-width: 50%;
+        background-color: white;
+        border-radius: 5px;
+
+    }
+
+    .achievement-list__item--locked {
+        border: 1px solid #999999;
+        background-color: #cccccc;
+        color: #666666;
+    }
+
+    .achievement-list__item--bronze {
+        border: 3px solid #cd7f32;
+    }
+
+    .achievement-list__item--silver {
+        border: 3px solid silver;
+    }
+
+    .achievement-list__item--gold {
+        border: 3px solid gold;
+    }
+
+    .achievement-list__item--brilliant {
+        border: 3px solid #B9F2FF;
     }
 
     .achievement__logo {
@@ -41,7 +81,7 @@
 
 <ul class="achievement-list">
     {#each list as achievement}
-        <li class="achievement-list__item achievement">
+        <li class={getItemClasses(achievement)}>
             <img class="achievement__logo" src={achievement.logo || DEFAULT_LOGO} alt={`${achievement.title} иконка`}>
             <div>
                 <div class="achievement__title">{achievement.title}</div>
