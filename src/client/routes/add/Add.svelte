aca05f141a05e255e72c7c7114f2765b3d2cf873
Egor Zaplatin <lawliet-94@mail.ru>
2019-08-07 20:21:45 +0300

    add achievement draft styling

--- src/client/routes/add/Add.svelte
+++ src/client/routes/add/Add.svelte
@@ -53,40 +53,57 @@
 
 </script>
 
+<style>
+  .add-achievement {
+    display: flex;
+    justify-content: center;
+  }
 
-<form on:submit={onSubmit} encType="multipart/form-data" bind:this={form}>
-  <label>
-    Название: <input type="text" name="title">
-  </label>
-  <label>
-    Описание: <input type="text" name="desc">
-  </label>
-  <label>
-    ID: <input type="text" name="id">
-  </label>
-  <label>
-  {#if logoImage}
-    <img src={logoImage} alt="logo-image" />
-  {/if} Logo: <input type="file" name="logo" on:change={onLogoChange}/>
-  </label>
+  .add-achievement__form {
+    min-width: 800px;
+    width: 50%;
+    margin: 20px;
+    padding: 10px;
+    box-shadow: 0 0 1px 0 black;
+  }
+</style>
+
+<main class="add-achievement">
+  <form  class="add-achievement__form" on:submit={onSubmit} encType="multipart/form-data" bind:this={form}>
+    <label>
+      Название: <input type="text" name="title">
+    </label>
+    <label>
+      Описание:<br />
+      <textarea name="desc"></textarea>
+    </label>
+    <label>
+      ID: <input type="text" name="id">
+    </label>
+    <label>
+      {#if logoImage}
+        <img src={logoImage} alt="logo-image" />
+      {/if} Logo: <input type="file" name="logo" on:change={onLogoChange}/>
+    </label>
 
     {#each photoImages as photoImage, i}
       <img src={photoImage} alt={`photo-image-${i}`} />
     {/each}
-  <label>
-     Photos: <input type="file" name="photos" multiple on:change={onPhotosChange}/>
-  </label>
+    <label>
+      Photos: <input type="file" name="photos" multiple on:change={onPhotosChange}/>
+    </label>
     <!-- add photos -->
-  <label>
-    Дата: <input type="date" name="date">
-  </label>
+    <label>
+      Дата: <input type="date" name="date">
+    </label>
 
-  <select name="type">
-    <option value="0" selected>Бронза</option>
-    <option value="1">Серебро</option>
-    <option value="2">Золото</option>
-    <option value="3">Платина</option>
-  </select>
+    <select name="type">
+      <option value="0" selected>Бронза</option>
+      <option value="1">Серебро</option>
+      <option value="2">Золото</option>
+      <option value="3">Платина</option>
+    </select>
 
-  <button type="submit" on:submit={onSubmit}>Отправить</button>
-</form>
+    <button type="submit" on:submit={onSubmit}>Отправить</button>
+  </form>
+</main>
