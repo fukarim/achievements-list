d2ff52309c9b27d9881fe7f9b33f6d780ec90e90
Egor Zaplatin <lawliet-94@mail.ru>
2019-08-18 21:35:52 +0300

    add modal component

---
+++ src/client/components/Modal.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let isOpen = false;
+  export let onClose = () => isOpen = false;
+
+  $: modalStyles = `display: ${isOpen ? 'block' : 'none'}`;
+
+  let modalContent;
+
+  function onClickOutside(e) {
+    if (e.target === modalContent) {
+      return false;
+    }
+
+    onClose();
+  }
+</script>
+
+<div class="modal" style={modalStyles} on:click={onClickOutside}>
+
+  <div class="modal__content" bind:this={modalContent}>
+    <span class="modal__close" on:click={onClose}>&times;</span>
+    <slot></slot>
+  </div>
+
+</div>
+
+<style>
+  .modal {
+    position: fixed;
+    z-index: 1;
+    padding-top: 100px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0,0,0,0.4);
+  }
+
+  .modal__content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+  }
+
+  .modal__close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  .modal__close:hover,
+  .modal__close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+  }
+</style>
